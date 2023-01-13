519f55053e45965a8b63aeda75b75d0dc2eb2404
Felix Lindner <felix@existenzquantor.info>
2023-01-13 21:38:38 +0100

    .

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.existenzquantor/pddl-parse-and-ground "0.1.6"
+(defproject org.clojars.existenzquantor/pddl-parse-and-ground "0.1.7"
   :description "A Parser and Grounder for PDDL files"
   :url "https://github.com/existenzquantor/pddl_parse_and_ground"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"] [org.clojure/data.json "2.4.0"] [org.clojure/math.combinatorics "0.1.7"]]
+  :dependencies [[org.clojure/clojure "1.11.1"] [org.clojure/data.json "2.4.0"] [org.clojure/math.combinatorics "0.1.6"]]
   :main ^:skip-aot pddl-parse-and-ground.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
